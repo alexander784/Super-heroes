882c1a5eca0e28a0dc284b3ce1174d163555d4ef
alexander <alexandernyaga1999@gmail.com>
2024-01-20 23:18:02 +0300

    Define the models

--- app/app.py
+++ app/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,jsonify
 from flask_migrate import Migrate
 
 from models import db, Hero
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
@@ -18,5 +18,7 @@ def home():
     return ''
 
 
+
+
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5550)
--- app/models.py
+++ app/models.py
@@ -1,10 +1,49 @@
+# models.py
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+class Hero(db.Model,SerializerMixin):
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    super_name = db.Column(db.String(255), nullable=False)
+    hero_powers = db.relationship('HeroPower', back_populates='hero')
 
-# add any models you may need. 
+class Power(db.Model,SerializerMixin):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    hero_powers = db.relationship('HeroPower', back_populates='power')
+
+    @validates('description',)
+    def validate_description(self, key, description):
+        if description and len(description.split()) < 3:
+           raise ValueError("Description must have at least 3 words.")
+        return description
+
+
+
+class HeroPower(db.Model,SerializerMixin):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String(255), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
+
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
+
+    @validates('strength')
+    def validate_strength(self, key, strength):
+        valid_strengths = ['Strong', 'Weak', 'Average']
+        if strength not in valid_strengths:
+            raise ValueError("Strength must be one of: 'Strong', 'Weak', 'Average'")
+        return strength
