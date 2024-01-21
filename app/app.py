1f41774ba4391b95919e1a0aa65ade1ae39fe552
alexander <alexandernyaga1999@gmail.com>
2024-01-21 08:32:04 +0300

    Add seed.py file

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///your_database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
